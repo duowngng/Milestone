fe0e5d18f4a11df9a5f3c28c56fb1e31244b815c
duowngng <duowng.ng@gmail.com>
2024-11-23 23:49:44 +0700

    feat: add auth middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  const { data, isLoading } = useCurrent();
+  const { mutate } = useLogout();
+
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-in");
+    }
+  }, [data]);
+
   return (
     <div>
-      <Button>Click me</Button>
+      Hello, user!
+      <Button onClick={() => mutate()}>Logout</Button>
     </div>
   );
 }
