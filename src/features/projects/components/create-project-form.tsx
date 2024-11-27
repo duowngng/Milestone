b716251908ce76ca84e2e1ab4b62b6b41c976629
duowngng <duowng.ng@gmail.com>
2024-11-27 22:59:37 +0700

    feat: add settings page, update, delete project

--- src/features/projects/components/create-project-form.tsx
+++ src/features/projects/components/create-project-form.tsx
@@ -54,9 +54,9 @@ export const CreateProjectForm = ({ onCancel }: CreateProjectFormProps) => {
     };
 
     mutate({ form: finalValues }, {
-      onSuccess: () => {
+      onSuccess: ({ data }) => {
         form.reset();
-        // Redirect to the new project page
+        router.push(`/workspaces/${workspaceId}/projects/${data.$id}`);
       }
     });
   };
