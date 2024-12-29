639fb52285b81bf51cc6974c18a4052804713ed0
duowngng <duowng.ng@gmail.com>
2024-12-29 21:07:28 +0700

    feat: add OAuth

--- src/features/tasks/server/route.ts
+++ src/features/tasks/server/route.ts
@@ -109,7 +109,7 @@ const app = new Hono()
 
           return {
             ...member,
-            name: user.name,
+            name: user.name || user.email,
             email: user.email,
           };
         }),
@@ -180,7 +180,7 @@ const app = new Hono()
 
       const assignee = {
         ...member,
-        name: user.name,
+        name: user.name || user.email,
         email: user.email,
       };
 
