6892f3fa0844e93d1aa39c8eb82618947c6bcd80
duowngng <duowng.ng@gmail.com>
2025-01-08 22:37:39 +0700

    fix: bug

--- src/features/tasks/components/task-breadcrumbs.tsx
+++ src/features/tasks/components/task-breadcrumbs.tsx
@@ -56,7 +56,7 @@ export const TaskBreadcrumbs = ({
         image={project.imageUrl}
         className="size-6 lg:size-8"
       />
-      <Link href={`workspaces/${workspaceId}/projects/${project.$id}`}>
+      <Link href={`/workspaces/${workspaceId}/projects/${project.$id}`}>
         <p className="text-sm lg:text-lg font-semibold text-muted-foreground hover:opacity-75 transition">
           {project.name}
         </p>
